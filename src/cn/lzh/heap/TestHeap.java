4feef7754d70a1abdb9f3595c9e3fcedf9e3734d
zhuanglee <devlzh93@gmail.com>
2019-01-15 15:59:45 +0800

    优化测试用例

--- src/cn/lzh/heap/TestHeap.java
+++ src/cn/lzh/heap/TestHeap.java
@@ -7,51 +7,38 @@ import java.util.Arrays;
 public class TestHeap {
 
     public static void main(String[] args) {
-        test1();
-        test2();
+        testSmallTopHeap();
+        testBigTopHeap();
     }
 
-    private static void test1() {
-        System.out.println("\ntest1:初始化空堆");
-        Heap<Integer> heap = new SmallTopHeap<>(9);
-        System.out.println("\ninit heap：" + heap.toString());
-        sort(heap);
-        // 删除+排序
-        if (heap.deleteTop() != null) {
-            System.out.println("\ndeleteTop：" + heap.toString());
-            sort(heap);
-        } else {
-            System.out.println("\ndeleteTop failed");
-        }
-        // 插入+排序
+    private static void testSmallTopHeap() {
+        System.out.println("\n******************* SmallTopHeap *******************\n");
         Integer[] data = new Integer[]{7, 5, 19, 8, 4, 1, 20, 13, 16, 2};
+        Heap<Integer> heap = new SmallTopHeap<>(data.length - 1);
+        // 插入+排序
         for (Integer i : data) {
             if (heap.insert(i)) {
                 System.out.printf("\ninsert(%d)：%s\n", i, heap.toString());
                 sort(heap);
             } else {
                 System.out.printf("\ninsert(%d)：failed\n", i);
+                break;
             }
         }
         // 删除+排序
-        if (heap.deleteTop() != null) {
-            System.out.println("\ndeleteTop：" + heap.toString());
-            sort(heap);
-        } else {
-            System.out.println("\ndeleteTop failed");
-        }
-        // 插入+排序
-        int i = 2;
-        if (heap.insert(i)) {
-            System.out.printf("\ninsert(%d)：%s\n", i, heap.toString());
-            sort(heap);
-        }else {
-            System.out.printf("\ninsert(%d)：failed\n", i);
+        while (true) {
+            if (heap.deleteTop() != null) {
+                System.out.println("\ndeleteTop：" + heap.toString());
+                sort(heap);
+            } else {
+                System.out.println("\ndeleteTop failed");
+                break;
+            }
         }
     }
 
-    private static void test2() {
-        System.out.println("\ntest2:根据数组数据创建堆");
+    private static void testBigTopHeap() {
+        System.out.println("\n******************* BigTopHeap *******************\n");
         Integer[] data = new Integer[]{7, 5, 19, 8, 4, 1, 20, 13, 16};
         System.out.println("init data：" + Arrays.toString(data));
         // 初始化+排序
@@ -63,7 +50,7 @@ public class TestHeap {
         if (heap.insert(i)) {
             System.out.printf("\ninsert(%d)：%s\n", i, heap.toString());
             sort(heap);
-        }else {
+        } else {
             System.out.printf("\ninsert(%d)：failed\n", i);
         }
         // 删除+排序
@@ -77,7 +64,7 @@ public class TestHeap {
         if (heap.insert(i)) {
             System.out.printf("\ninsert(%d)：%s\n", i, heap.toString());
             sort(heap);
-        }else {
+        } else {
             System.out.printf("\ninsert(%d)：failed\n", i);
         }
         // 插入+排序
@@ -85,14 +72,13 @@ public class TestHeap {
         if (heap.insert(i)) {
             System.out.printf("\ninsert(%d)：%s\n", i, heap.toString());
             sort(heap);
-        }else {
+        } else {
             System.out.printf("\ninsert(%d)：failed\n", i);
         }
     }
 
     private static void sort(Heap<Integer> heap) {
-        heap.sort();
-        System.out.println("heap.sort：" + CommonUtils.toString(heap.getData(), true));
+        System.out.println("heap.sort：" + CommonUtils.toString(heap.sort(), true));
     }
 
 }
